934fb63404543c1cd6ae40b43d1bfc19394e8da1
Tim Peters <mail@darksecond.nl>
2014-05-01 21:52:26 +0200

    added math namespace

--- include/math/matrix_types.h
+++ include/math/matrix_types.h
@@ -1,34 +1,37 @@
 #pragma once
 
-template<typename T, int Rows, int Cols>
-struct matrix
+namespace math
 {
-	static_assert(Cols > 0, "There must be at least one column");
-	static_assert(Rows > 0, "There must be at least one row");
+    template<typename T, int Rows, int Cols>
+    struct matrix
+    {
+        static_assert(Cols > 0, "There must be at least one column");
+        static_assert(Rows > 0, "There must be at least one row");
 
-	static constexpr int rows = Rows;
-	static constexpr int cols = Cols;
-	typedef T type;
-	typedef vector<T, Cols> row;
-	//TODO typedef vector<T, Rows> column; (unused right now)
-	row data[Rows];
+        static constexpr int rows = Rows;
+        static constexpr int cols = Cols;
+        typedef T type;
+        typedef vector<T, Cols> row;
+        //TODO typedef vector<T, Rows> column; (unused right now)
+        row data[Rows];
 
-	row& operator[](const int pos) { return data[pos]; }
-	constexpr row operator[](const int pos) const { return data[pos]; }
+        row& operator[](const int pos) { return data[pos]; }
+        constexpr row operator[](const int pos) const { return data[pos]; }
 
-	/**
-	 * Create an identity matrix.
-	 * The matrix requested must be square.
-	 */
-	static matrix<T, Rows, Cols> identity()
-	{
-		static_assert(Rows == Cols, "Matrix must be square");
+        /**
+         * Create an identity matrix.
+         * The matrix requested must be square.
+         */
+        static matrix<T, Rows, Cols> identity()
+        {
+            static_assert(Rows == Cols, "Matrix must be square");
 
-		matrix<T, Rows, Cols> result;
-		for(int m=0;m<Rows;++m)
-		{
-			result[m][m] = 1;
-		}
-		return result;
-	}
-};
+            matrix<T, Rows, Cols> result;
+            for(int m=0;m<Rows;++m)
+            {
+                result[m][m] = 1;
+            }
+            return result;
+        }
+    };
+}
--- sample/math/math_sample.cc
+++ sample/math/math_sample.cc
@@ -3,8 +3,9 @@
 
 int main()
 {
-    mat2x2 c = mat2x2::identity();
+    math::mat2x2 c = math::mat2x2::identity();
     printf("[%f, %f]\n", c[0][0], c[0][1]);
     printf("[%f, %f]\n", c[1][0], c[1][1]);
+
     return 0;
 }
